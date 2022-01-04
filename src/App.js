22fe816d2bd93d0df21e9c98e80da28920e050d4
amirbenasr <amirbennasr@gmail.com>
2022-01-05 00:38:32 +0100

    add image

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Container from "./Components/Container";
 import Footer from "./Components/Footer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Components/HomePage";
+import Login from "./Components/Login";
+import About from "./Components/About";
 
 function App() {
   return (
@@ -11,7 +13,9 @@ function App() {
         <Navbar></Navbar>
 
         <Routes>
-          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/" element={<HomePage/>}></Route>
+          <Route path="/login" element={<Login/>}></Route>
+          <Route path="/about" element={<About/>}></Route>
         </Routes>
       </Router>
     </div>
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,48 +1,54 @@
-import { Button, Container } from "@mui/material";
+import { Button } from "@mui/material";
 import React from "react";
 import "../Styles/homepage.css";
-
+import image from "../images/female-student.jpg";
+import Container from "./Container";
 import TechnologyCard from "./TechnologyCard";
 
 function HomePage() {
   return (
-    <Container>
+    <div className="container">
+
       <div className="homepage">
+        <img src={image} />
+
         <div className="onboarding-text">
           <p>
-            Get your academic project done <i>in Time</i>
+            Get your academic project done <br></br> <i>in Time</i>
           </p>
+        <Button variant="contained">Get Started</Button>
+
         </div>
+
         <div className="tech-list">
-          <TechnologyCard
+          {/* <TechnologyCard
             image={
               "https://image.shutterstock.com/image-vector/front-end-development-web-application-260nw-521164462.jpg"
             }
             specialty={"Web-Project"}
-          ></TechnologyCard>
-          <TechnologyCard
+            ></TechnologyCard>
+            <TechnologyCard
             image={
               "https://www.editorinsidetechnology.com/images/digiicon/mobile-development.png"
             }
             specialty={"Mobile-Project"}
-          ></TechnologyCard>
-          <TechnologyCard
+            ></TechnologyCard>
+            <TechnologyCard
             image={
               "https://thumbs.dreamstime.com/z/backend-development-doodle-back-end-icon-website-cteation-hand-drawn-vector-illustration-139983107.jpg"
             }
             specialty={"Back-End-Project"}
-          ></TechnologyCard>
-          <TechnologyCard
+            ></TechnologyCard>
+            <TechnologyCard
             image={
               "https://thumbs.dreamstime.com/b/vector-illustration-full-stack-developer-working-computer-professional-programmer-coding-website-creation-proccess-172914142.jpg"
             }
             specialty={"FullStack-Project"}
-          ></TechnologyCard>
+          ></TechnologyCard> */}
         </div>
 
-        <Button variant="contained">Get Started</Button>
       </div>
-    </Container>
+          </div>
   );
 }
 
