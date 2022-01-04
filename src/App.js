f82cd906ed411874a4323c14fa23ac4f972f8a4c
amirbenasr <amirbennasr@gmail.com>
2022-01-04 01:16:54 +0100

    change styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Navbar from "./Components/Navbar";
 import Container from "./Components/Container";
+import Footer from "./Components/Footer";
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
       <Container></Container>
-      {/* <Navbar></Navbar> */}
+      <Footer></Footer>
 
     </div>
   );
